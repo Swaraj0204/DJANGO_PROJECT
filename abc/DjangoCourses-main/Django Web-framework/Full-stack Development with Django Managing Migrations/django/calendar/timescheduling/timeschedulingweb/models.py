ee3b0439f76d3aa194301a9d503cd437d70bd2ab
Swaraj Singh Gour <gourswarajsingh@gmail.com>
2025-03-23 01:35:53 +0530

    Delete abc/DjangoCourses-main/Django Web-framework/Full-stack Development with Django Managing Migrations/django directory

--- abc/DjangoCourses-main/Django Web-framework/Full-stack Development with Django Managing Migrations/django/calendar/timescheduling/timeschedulingweb/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TimeSlot(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    description = models.TextField()
